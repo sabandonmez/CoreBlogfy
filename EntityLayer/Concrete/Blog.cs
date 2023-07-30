1cfdb3b5de615b0685faac8afaca960faabe2280
Şaban Dönmez <eren.donmez.11@gmail.com>
2023-07-30 23:08:15 +0300

    Proje dosyası ekle.

---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogId { get; set; }
+        public String BlogTitle { get; set; }
+        public String BlogContent { get; set; }
+        public String BlogThumbnailImage { get; set; }
+        public String BlogImage { get; set; }
+        public DateTime BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+
+
+    }
+}
---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public  class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public String CommentUserName { get; set; }
+        public String CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+        public bool CommentStatus { get; set; }
+
+    }
+}
